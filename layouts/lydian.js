1f916843a926a87cebe09bedf6358f422334f3ff
likethemagician <likethemagician@gmail.com>
2018-03-05 14:51:28 -0500

    Create lydian.js

---
+++ layouts/lydian.js
@@ -0,0 +1,12 @@
+/* Lydian  Keyboard Layout */
+jQuery.keyboard.layouts['lydian'] = {
+	"name" : "lydian",
+	"lang" : ["xld"],
+	"normal" : [
+		"` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+		"{tab} ; \u10932 \u10936 \u10924 \u1092D \u1092F \u10927 \u10930 \u03BF \u03C0 [ ]",
+		"\u03B1 \u03C3 \u03B4 \u03C6 \u03B3 \u03B7 \u03BE \u03BA \u03BB \u0384 ' \\ {enter}",
+		"{shift} < \u03B6 \u03C7 \u03C8 \u03C9 \u03B2 \u03BD \u03BC , . / {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	]
+	};
